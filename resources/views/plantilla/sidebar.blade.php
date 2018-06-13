7e6886fc5f78b1a984346ba7613a0d28254e4b50
Andrés Felipe Cardozo Cañas <andresf_ca1996@hotmail.com>
2018-06-12 22:55:07 -0500

    Agregando componente de listado de categoria

--- resources/views/plantilla/sidebar.blade.php
+++ resources/views/plantilla/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="sidebar">
-            <nav class="sidebar-nav">
+    <nav class="sidebar-nav">
                 <ul class="nav">
                     <li @click="menu=0" class="nav-item">
                         <a class="nav-link active" href="#"><i class="icon-speedometer"></i> Escritorio</a>
@@ -70,5 +70,5 @@
                     </li>
                 </ul>
             </nav>
-            <button class="sidebar-minimizer brand-minimizer" type="button"></button>
-        </div>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
